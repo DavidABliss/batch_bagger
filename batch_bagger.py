4228eb26d5ef557df42c160d34e8093009d1f2f2
DavidABliss <davidbliss@utexas.edu>
2021-12-17 15:11:40 -0600

    Update batch_bagger.py

--- batch_bagger.py
+++ batch_bagger.py
@@ -1,50 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-This script compiles folders in a target directory into individual bags. Users
-supply a target directory, a bag-info.txt template file, and a CSV spreadsheet
-containing bag-level information. The script reads the CSV spreadsheet, matches
-each entry to a bag in the target directory, and builds a bag-info file using
-the bag-info template file, supplemented with any bag-specific information 
-provided in the spreadsheet.
-
-At a minimum, the CSV spreadsheet must have a column headers row, and the first 
-column must list the names of the folders to be bagged within the target 
-directory. In this case, the only difference between the bag-info template and 
-the bag-info file in each bag will be the addition of a UUID 
-External-Identifier
-
-To supplement the bag-info template file with bag-specific information, enclose 
-placeholder text in the bag-info template within [[double brackets]]. Create a 
-column with a header matching that text (minus the brackets) in the CSV 
-spreadsheet, and put bag-specific replacement text in that column. The script 
-will match the placeholder text to the column header and insert the 
-corresponding value into the output bag-info.txt file.
-
-Values in the bag-info template file can be split across multiple lines. 
-The script will combine these values into a single line for the output 
-bag-info.txt file.
-
-Fields in the bag-info template file can repeat. bagit.py does not allow fields
-to be repeated, so each value will be combind into a single line, separated by
-pipes.
-
-The script assigns a UUID External-Identifier to each bag, but allows for
-existing External-Identifiers to be provided alongside this.
-
-Users can add the -v (verbose) switch to print the contents of the bag-info
-file as each bag is created
-
-All bags are created in place.
-
-Finally, the script outputs a list of bags that were created, alongside the 
-UUID assigned to each, to a text file. This is meant for recordkeeping, but can
-be modified or removed as needed.
-
-This script is written for the UT Libraries' local bag-info spec, and will not
-recognize any terms not found in fieldsList as labels for the output 
-bag-info.txt files. The script can be made to work with other bag-info specs by 
-modifying that list.
-"""
 
 import bagit
 import sys
@@ -62,7 +16,7 @@ try:
     bagInfo = sys.argv[2]
     spreadSheet = sys.argv[3]
 except:
-    sys.exit("Usage: python batch_bagger.py <path to bags folder> <path to bag-info txt template> <path to bag-info CSV spreadsheet>")
+    sys.exit("Usage: python batch_bagger.py <path to bags folder> <path to bag-info txt template> <path to bag-info CSV or XLSX spreadsheet>")
 
 # Allow users to add a -v switch, which will print each bag-info file as it is written
 try:
